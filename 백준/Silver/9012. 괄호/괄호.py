cba30b6ef412c4540c741684d746ddef8bcd5a63
sonminseung <105152528+sonminseung@users.noreply.github.com>
2022-08-22 21:32:28 +0900

    [Silver IV] Title: 괄호, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,17 @@
+T = int(input())
+for i in range(T):
+    b = input()
+    s = list(b)
+    sum = 0
+    for i in s:
+        if i == '(':
+            sum += 1
+        elif i == ')':
+            sum -= 1
+        if sum < 0:
+            print('NO')
+            break
+    if sum > 0:
+        print('NO')
+    elif sum == 0:
+        print('YES')
